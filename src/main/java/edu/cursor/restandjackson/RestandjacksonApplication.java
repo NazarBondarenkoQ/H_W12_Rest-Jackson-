89b9e1bda7748cc55886e6f0682b2589ddd76f28
NazarBondarenkoQ <nazarbondarenko1@gmail.com>
2019-08-08 19:52:34 +0300

    Initial commit.

---
+++ src/main/java/edu/cursor/restandjackson/RestandjacksonApplication.java
@@ -0,0 +1,13 @@
+package edu.cursor.restandjackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestandjacksonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestandjacksonApplication.class, args);
+    }
+
+}
